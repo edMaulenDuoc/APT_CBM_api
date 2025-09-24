d7853eefbcaabdfe4f250a2d2a5b4b3e933c55c8
edMaulenDuoc <ed.maulen@duocuc.cl>
2025-09-24 16:56:12 -0300

    creacion de ruta estado  para comprobar el estado general de la api, se usa para mostrar alerta en el frontend

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,23 @@ import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
 import catalogoRoutes from "./catalogos.routes.js";
 import emergenciaRoutes from "./emergencia.routes.js";
-
+import prisma from "../config/db.js";
 const router = Router();
 
 router.use("/users", userRoutes);
 router.use("/auth", authRoutes);
 router.use("/catalogos", catalogoRoutes);
 router.use("/emergencias", emergenciaRoutes);
+
+
+// Ruta de estado para verificar si la API está funcionando
+router.get("/estado", async (req, res) => {
+    try {
+        await prisma.tipoUsuario.findMany();
+        return res.status(200).json({ estado: "API funcionando correctamente" });
+    } catch (error) {
+        return res.status(500).json({ estado: "Error en la conexión a la API" });
+    } 
+});
 
 export default router;
